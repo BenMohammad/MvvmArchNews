77e0e6a6f578852ae48a17f6877596adec5e5df3
Ben Mohammad <benmohammad593@gmail.com>
2020-06-28 20:58:54 +0100

    extensions package added....

---
+++ app/src/main/java/com/benmohammad/mvvmarchnews/utils/extensions/ViewExtensions.kt
@@ -0,0 +1,45 @@
+package com.benmohammad.mvvmarchnews.utils.extensions
+
+import android.content.Context
+import android.graphics.PorterDuff
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.LayoutRes
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
+
+fun View.hone() {
+    visibility = View.GONE
+}
+
+fun Context.getColorCompat(@ColorRes colorRes: Int) = ContextCompat.getColor(this, colorRes)
+fun Fragment.getColor(@ColorRes colorRes: Int) = ContextCompat.getColor(requireContext(), colorRes)
+
+fun FragmentActivity.toast(text: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, text, duration).show()
+}
+
+fun ViewGroup.inflate(layoutRes: Int): View {
+    return LayoutInflater.from(context).inflate(layoutRes, this, false)
+}
+
+fun Context.getDrawableCompat(@DrawableRes resId: Int, @ColorRes tintColor: Int = 0) = when {
+    tintColor != 0 -> AppCompatResources.getDrawable(this, resId)?.apply {
+        setColorFilter(getColorCompat(tintColor), PorterDuff.Mode.SRC_ATOP)
+    }
+    else -> AppCompatResources.getDrawable(this, resId)
+}
