1dd74d92ef5ad0d992aa123ab24c56ae3e7056d0
Ben Mohammad <benmohammad593@gmail.com>
2020-06-28 07:06:11 +0100

    Initial commit

---
+++ app/src/main/java/com/benmohammad/mvvmarchnews/CountryListingActivity.kt
@@ -0,0 +1,12 @@
+package com.benmohammad.mvvmarchnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CountryListingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_countries)
+    }
+}
